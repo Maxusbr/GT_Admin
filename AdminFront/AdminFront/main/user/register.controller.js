d8a775512e6f6ee0d3e483c887287d73ffec8bec
Alexander <alex.v.belozerov@gmail.com>
2016-05-31 01:16:36 +0300

    update admin
    update admin publish proj

--- AdminFront/AdminFront/main/user/register.controller.js
+++ AdminFront/AdminFront/main/user/register.controller.js
@@ -5,9 +5,57 @@
         .module('app')
         .controller('MainUserRegisterController', MainUserRegisterController);
 
-    MainUserRegisterController.$inject = ['UserService', '$rootScope'];
-    function MainUserRegisterController(UserService, $rootScope) {
+    MainUserRegisterController.$inject = ['UserService', '$scope'];
+    function MainUserRegisterController(UserService, $scope) {
         var vm = this;
+
+        $scope.submitted = false;
+        $scope.passtype = 'password';
+        $scope.user = {};
+        $scope.roles = [
+            {id: 1, name: 'R1'},
+            {id: 2, name: 'R2'},
+            {id: 3, name: 'R3'},
+            {id: 4, name: 'R4'},
+        ];
+
+        $scope.register = register;
+        $scope.generatePassword = generatePassword;
+
+
+        function str_rand() {
+            var result = '';
+            var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+            var i;
+            var position;
+            var max_position = words.length - 1;
+            for (i = 0; i < 10; ++i) {
+                position = Math.floor(Math.random() * max_position);
+                result = result + words.substring(position, position + 1);
+            }
+            return result;
+        }
+
+        function generatePassword(c) {
+            if ($('#generatePassword').is(':checked')) {
+                // $('#password').attr('type', 'text');
+                // $('#password').attr('value', str_rand());
+                $scope.passtype = 'text';
+                $scope.user.password = str_rand();
+            }
+            else {
+                // $('#password').attr('type', 'password');
+                // $('#password').attr('value', '');
+                $scope.passtype = 'password';
+                $scope.user.password = '';
+            }
+        };
+        function register(user) {
+            $scope.user = angular.copy(user);
+            $scope.submitted = true;
+
+            console.log(user);
+        };
 
     }
 
--- AdminFront/AdminFront/register/register.controller.js
+++ AdminFront/AdminFront/register/register.controller.js
@@ -9,7 +9,36 @@
     function RegisterController(UserService, $location, $rootScope, FlashService) {
         var vm = this;
 
+        vm.user = {};
+        vm.passtype = 'password';
+
         vm.register = register;
+        vm.generatePassword = generatePassword;
+
+
+        function str_rand() {
+            var result = '';
+            var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+            var i;
+            var position;
+            var max_position = words.length - 1;
+            for (i = 0; i < 10; ++i) {
+                position = Math.floor(Math.random() * max_position);
+                result = result + words.substring(position, position + 1);
+            }
+            return result;
+        }
+
+        function generatePassword() {
+            if ($('#generatePassword').is(':checked')) {
+                vm.passtype = 'text';
+                vm.user.password = str_rand();
+            }
+            else {
+                vm.passtype = 'password';
+                vm.user.password = '';
+            }
+        };
 
         function register() {
             vm.dataLoading = true;
