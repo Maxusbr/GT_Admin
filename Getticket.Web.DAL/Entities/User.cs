ceef17ae1225b8c152110fe83226807adcde111a
MariStor <mari_yar@inbox.ru>
2016-06-17 14:58:02 +0300

    Пересмотр структуры проекта

--- Getticket.Web.DAL/Entities/User.cs
+++ Getticket.Web.DAL/Entities/User.cs
@@ -18,10 +18,14 @@ namespace Getticket.Web.DAL.Entities
         /// </summary>
         public virtual UserInfo UserInfo { get; set; }
 
-
         /// <summary>
         /// Роль доступа пользователя к API
         /// </summary>
         public virtual AccessRole AccessRole { get; set; }
+
+        /// <summary>
+        /// Статус пользователя в системе
+        /// </summary>
+        public virtual UserStatus UserStatus { get; set; }
     }
 }
