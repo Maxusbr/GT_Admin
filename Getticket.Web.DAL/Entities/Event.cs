192ca50b50ffa6d5226caaeb178ccec2db9705a7
MariStor <mari_yar@inbox.ru>
2016-06-29 16:05:39 +0300

    Заготовка Сущностей для "Событий"

---
+++ Getticket.Web.DAL/Entities/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Getticket.Web.DAL.Entities
+{
+    /// <summary>
+    /// Сущность для хранения событий
+    /// </summary>
+    public class Event : BaseEntity
+    {
+        /// <summary>
+        /// Имя события
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+    }
+}
