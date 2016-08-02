66d5f74822ccfa5493cdc22f6c4985792f87b8cf
Maxusbr <max_73@inbox.ru>
2016-08-02 10:18:50 +0300

    fix person create overview

--- AdminFront/main/persona/create/schema.controller.js
+++ AdminFront/main/persona/create/schema.controller.js
@@ -12,7 +12,7 @@
             $scope.tizerlist = [];
             var item = $filter('filter')(data, { Type: 1 })[0];
             if (item != undefined) {
-                item.List.forEach(function(el) {
+                item.List.forEach(function (el) {
                     el.Name = el.DescriptionText.length > 28 ? el.DescriptionText.substr(0, 25) + '...' : el.DescriptionText;
                     $scope.tizerlist.push(el);
                 });
@@ -20,30 +20,39 @@
         }
         var indx = 0;
         var fact = { id: indx++, Tags: [], IdPerson: $scope.id };
+        $scope.dimension = "см";
         $scope.factlist = [];
         $scope.factlist.push(fact);
         $scope.existDescription = true;
         function collectLastFact() {
-            fact.IdAntroName = $scope.IdAntroName;
+            fact.IdAntroName = $scope.antro.Id;
+            fact.AntroName = $scope.antro.Name;
             fact.IsMoreThan = $scope.IsMoreThan;
             fact.Value = $scope.FeatureValue;
+            fact.dimension = $scope.dimension;
         }
 
 
-        $scope.tizer = { id_DescriptionType: 1, DescriptionText: "", id_Person: $scope.id};
+        $scope.tizer = { id_DescriptionType: 1, DescriptionText: "", id_Person: $scope.id };
         personService.getDescript($scope.id, getDescript);
 
-        personService.getTags(function(data) {
+        personService.getTags(function (data) {
             $scope.tags = [];
             $scope.tags.push.apply($scope.tags, data);
         });
         $scope.FeatureName = "Антропометрия";
         $scope.loadTags = function (query) {
-            return $scope.tags.filter(function(item) { return item.Name.toLowerCase().indexOf(query.toLowerCase()) >= 0; });
+            var result = $scope.tags.filter(function (item) { return item.Name.toLowerCase().indexOf(query.toLowerCase()) >= 0; });
+            result = $filter('orderBy')(result, function (item) {
+                item.Name.substring(0, query.length);
+            });
+            return result;
         }
         $scope.showOverwiew = function () {
             $rootScope.tizer = $scope.tizer;
+            $rootScope.tizerTags = $scope.TizerTags;
             $rootScope.staticDescription = $scope.staticDescription;
+            $rootScope.factlist = $scope.factlist;
             collectLastFact();
             var result = {
                 IdPerson: $scope.id,
@@ -54,9 +63,8 @@
                     StaticDescription: $scope.staticDescription
                 },
                 TagAntroList: $scope.factlist
-        }
-
-            personService.saveDescriptions(result, function(data) {
+            }
+            personService.saveDescriptions(result, function (data) {
                 if (data.Message)
                     $scope.response = data.Message;
                 else {
@@ -67,10 +75,16 @@
 
     }
 
-    
+
     angular
         .module('app')
-        .controller('MainPersonaCreateSchemaController', MainPersonaCreateSchemaController);
+        .controller('MainPersonaCreateSchemaController', MainPersonaCreateSchemaController)
+        .filter('join', function () {
+            return function (arr, glue) {
+                if (!Array.isArray(arr)) { return arr; }
+                return arr.join(glue);
+            };
+        });
 
     MainPersonaCreateSchemaController.$inject = ['$scope', '$rootScope', 'personService', '$location', '$filter'];
 })();
