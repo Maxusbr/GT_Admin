9506c652292c4980dabe76bdc404ed83064478e1
Maxusbr <max_73@inbox.ru>
2016-08-11 10:30:33 +0300

    fix found Country Places

--- Getticket.Web.DAL/Entities/CountryPlaces.cs
+++ Getticket.Web.DAL/Entities/CountryPlaces.cs
@@ -20,21 +20,19 @@ namespace Getticket.Web.DAL.Entities
         /// </summary>
         [MaxLength(500)]
         [Required]
-        [Index("CountryPlaceNameIndex", 1, IsUnique = true)]
+        [Index("CountryPlaceNameIndex")]
         public string Name { get; set; }
 
         /// <summary>
         /// Название
         /// </summary>
         [MaxLength(50)]
-        [Index("CountryPlaceNameIndex", 2, IsUnique = true)]
         public string Abr { get; set; }
 
         /// <summary>
         /// Внешний ключ для <see cref="Entities.Region"/>
         /// </summary>
         [Required]
-        [Index("CountryPlaceNameIndex", 3, IsUnique = true)]
         public int id_Region { get; set; }
 
         /// <summary>
