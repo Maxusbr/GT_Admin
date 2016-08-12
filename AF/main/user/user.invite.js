367b5216c1e3d41116493dc80e2a88be90399c6e
Alexander <alex.v.belozerov@gmail.com>
2016-08-12 10:27:49 +0300

    add lost file

---
+++ AF/main/user/user.invite.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('UserInviteController', UserInviteController);
+
+    UserInviteController.$inject = ['$rootScope'];
+    function UserInviteController($rootScope) {
+        var vm = this;
+    }
+})();
