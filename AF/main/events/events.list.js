a43e5409a1a9feb8214f8cd89e62e9e7324513ab
gitwhatislove <whatislove0505@gmail.com>
2016-08-16 01:07:20 +0300

    'add'

--- AF/main/events/events.list.js
+++ AF/main/events/events.list.js
@@ -1,1 +1,27 @@
 ﻿
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('EventListController', EventListController);
+
+    EventListController.$inject = ['$rootScope', '$scope', 'personService', '$interval'];
+    function EventListController($rootScope, $scope, personService, $interval) {
+        var vm = this;
+        $scope.Promise = personService.getPersons();
+        $rootScope.createEvent = function create_event() {
+            console.log('create event');
+            app.closeSecond();
+            app.loadContentView('/main/events/events.create.html', 1800);
+        }
+
+        $scope.selectEvents = function (id) {
+            console.log(id);
+            $rootScope.Id = id;
+            app.closeSecond();
+            app.loadContentView('/main/events/events.viewone.html', 1800);
+        }
+    }
+})();
---
+++ AF/main/events/r3/r4/person.notes.static.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    function personNotesStaticController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+
+        $rootScope.savePersonNotesStatic = function save_person_notes_static(){
+            
+        }
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesStaticController', personNotesStaticController);
+
+    personNotesStaticController.$inject = ['$rootScope', '$cookieStore'];
+})();
