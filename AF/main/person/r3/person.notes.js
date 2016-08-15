d746a00225c552201e73bb79d6836faee1449768
Alexander <alex.v.belozerov@gmail.com>
2016-08-15 22:51:05 +0300

    add person description blank pages

--- AF/main/person/r3/person.notes.js
+++ AF/main/person/r3/person.notes.js
@@ -15,6 +15,16 @@
             app.closeFour();
             app.loadContentView('/main/person/r3/r4/person.notes.static.html', 3200);
         }
+
+        $rootScope.displayNotesTizer = function display_notes_tizer(){
+            app.closeFour();
+            app.loadContentView('/main/person/r3/r4/person.notes.tizer.html', 3200);
+        }
+
+        $rootScope.displayNotesAdaptDescription = function display_notes_adapt_description(){
+            app.closeFour();
+            app.loadContentView('/main/person/r3/r4/person.notes.ades.html', 3200);
+        }
     }
 
     angular
