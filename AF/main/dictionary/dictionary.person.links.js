a0d58bc738bf759af8b10375e7d17719c295f122
Alexander <alex.v.belozerov@gmail.com>
2016-08-15 17:29:19 +0300

    +++ dictionary base

--- AF/main/dictionary/dictionary.person.links.js
+++ AF/main/dictionary/dictionary.person.links.js
@@ -1,1 +1,15 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    function baseController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+    }
+
+    angular
+        .module('app')
+        .controller('baseController', baseController);
+
+    baseController.$inject = ['$rootScope', '$cookieStore'];
+})();
