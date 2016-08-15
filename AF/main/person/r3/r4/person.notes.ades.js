db85cbd94bd09098306af438e3e3d5b25b0892a8
Alexander <alex.v.belozerov@gmail.com>
2016-08-15 23:19:27 +0300

    update person notes tizer view

--- AF/main/person/r3/r4/person.notes.ades.js
+++ AF/main/person/r3/r4/person.notes.ades.js
@@ -1,1 +1,15 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    function personNotesAdesController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesAdesController', personNotesAdesController);
+
+    personNotesAdesController.$inject = ['$rootScope', '$cookieStore'];
+})();
--- AF/main/person/r3/r4/person.notes.tizer.js
+++ AF/main/person/r3/r4/person.notes.tizer.js
@@ -1,1 +1,21 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    function personNotesTizerController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+
+        $rootScope.savePersonNotesTizer = function save_person_notes_tizer(){
+            //TODO: save changes
+            //TODO: update notes table
+            //TODO: close window
+        }
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesTizerController', personNotesTizerController);
+
+    personNotesTizerController.$inject = ['$rootScope', '$cookieStore'];
+})();
