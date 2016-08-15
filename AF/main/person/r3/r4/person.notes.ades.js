c65cd51b4ae2e89f9ae9cafcc302fa868d5b44b1
Alexander <alex.v.belozerov@gmail.com>
2016-08-16 00:45:56 +0300

    add display person notes ades edit

--- AF/main/person/r3/r4/person.notes.ades.js
+++ AF/main/person/r3/r4/person.notes.ades.js
@@ -5,6 +5,11 @@
         var vm = this;
         if (!$rootScope.UserName)
             $rootScope.UserName = $cookieStore.get('username');
+
+        $rootScope.addPersonNotesAdapt = function add_person_notes_adapt(){
+            app.closeFive();
+            app.loadContentView('/main/person/r3/r4/r5/person.notes.ades.edit.html', 3800);
+        }
     }
 
     angular
