a5f1b1824a4f376ca54432d7ab9bb5568490f596
Alexander <alex.v.belozerov@gmail.com>
2016-08-15 18:38:12 +0300

    update person notes source

---
+++ AF/main/person/r3/r4/person.notes.source.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    function personNotesSourceController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+
+        $rootScope.savePersonNotesSource = function save_person_notes_source() {
+            //TODO: save changes or create new source
+            //TODO: update notes list
+            //TODO: close this window
+        }
+            
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesSourceController', personNotesSourceController);
+
+    personNotesSourceController.$inject = ['$rootScope', '$cookieStore'];
+})();
