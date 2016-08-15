6a261962b8149b2a2ce5062a4508f07454e77c5a
Alexander <alex.v.belozerov@gmail.com>
2016-08-16 01:15:11 +0300

    add person dic

---
+++ AF/main/dictionary/dictionary.person.wtf.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    function DictionaryPersonWTFController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+
+        $rootScope.dictionaryWTF = [
+            {
+                Id:1,
+                Name:'facebook'
+            }
+        ];
+
+        $rootScope.editItem;
+
+        $rootScope.saveChanges = function save_changes() {
+            //TODO: save changes to server
+            //TODO: refresh dictionary list
+        }
+    }
+
+    angular
+        .module('app')
+        .controller('DictionaryPersonWTFController', DictionaryPersonWTFController);
+
+    DictionaryPersonWTFController.$inject = ['$rootScope', '$cookieStore'];
+})();
