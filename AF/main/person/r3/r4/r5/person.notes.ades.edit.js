f01b5d425b26a1638be8cb4896e524897d95e0de
Alexander <alex.v.belozerov@gmail.com>
2016-08-16 00:35:29 +0300

    add edit person notes ades

---
+++ AF/main/person/r3/r4/r5/person.notes.ades.edit.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    function personNotesAdesEditController($rootScope, $cookieStore) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesAdesEditController', personNotesAdesEditController);
+
+    personNotesAdesEditController.$inject = ['$rootScope', '$cookieStore'];
+})();
