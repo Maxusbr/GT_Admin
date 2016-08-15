1e63f4c572b23b05966eed15d34f823125f77180
Maxusbr <max_73@inbox.ru>
2016-08-15 18:43:29 +0300

    fix select Bithplace

--- AF/main/person/person.edit.js
+++ AF/main/person/person.edit.js
@@ -8,7 +8,16 @@
             $scope.person = data;
             $scope.person.bithday = new Date($scope.person.Bithday);
         });
-        $scope.sexes = [{id: 0, name: "Мужской"},{id: 1, name: "Женский"}];
+        $scope.sexes = [{ id: 0, name: "Мужской" }, { id: 1, name: "Женский" }];
+        $scope.getPlaces = function (value) {
+            $scope.places = [];
+            return personService.getPlaces(value).then(function (response) {
+                $scope.places.push.apply($scope.places, response.data);
+                return response.data.map(function (item) {
+                    return item;
+                });
+            });
+        }
     }
 
     angular
