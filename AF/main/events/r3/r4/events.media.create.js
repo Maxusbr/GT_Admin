96f9e432bf6308624cf95727e37ef93fdee9ce28
gitwhatislove <whatislove0505@gmail.com>
2016-08-16 19:58:19 +0300

    'preview_img'

--- AF/main/events/r3/r4/events.media.create.js
+++ AF/main/events/r3/r4/events.media.create.js
@@ -5,20 +5,16 @@
         var vm = this;
         $scope.foto_ = false;
         $scope.video_ = true;
-        // $scope.personTags;
+        
         $scope.association = {};
         $scope.association.type = 'event';
-        // console.log($scope.association);
+
 
         $scope.showSelect = function show_select(type, check_type) {
-        	console.log(type);
-        	console.log(check_type);
             if (type == check_type) return true;
             else return false;
         }
-        $scope.changeFile = function changeFile() {
-        	console.log(vm.file);
-        }
+        
         
     }
 
