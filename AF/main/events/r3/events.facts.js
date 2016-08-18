7b63722f707cc9c482deea46fd31ec8d993836a3
gitwhatislove <whatislove0505@gmail.com>
2016-08-18 14:02:55 +0300

    'fix_events_fact'

--- AF/main/events/r3/events.facts.js
+++ AF/main/events/r3/events.facts.js
@@ -1,10 +1,10 @@
 ﻿(function () {
     'use strict';
 
-    function EventFactsController($rootScope, $scope, personService) {
+    function EventFactsController($rootScope, $scope, eventService) {
         var vm = this;
 
-        personService.getFact($rootScope.personId, function (data) {
+        eventService.getFact($rootScope.eventId, function (data) {
             $scope.factlist = [];
             data.forEach(function (item) {
                 $scope.factlist.push.apply($scope.factlist, item.List);
@@ -13,11 +13,11 @@
 
         $rootScope.addFact = function addFact(){
             app.closeFour();
-            app.loadContentView('/main/person/r3/r4/peron.fact.create.html', 3200);
+            app.loadContentView('/main/events/r3/r4/events.fact.edit.html', 3200);
         }
         $rootScope.editFact = function addFact(){
             app.closeFour();
-            app.loadContentView('/main/person/r3/r4/peron.fact.editor.html', 3200);
+            app.loadContentView('/main/events/r3/r4/events.fact.edit.html', 3200);
         }
 
     }
---
+++ AF/main/events/r3/r4/events.notes.source.js
@@ -0,0 +1,122 @@
+﻿(function () {
+    'use strict';
+
+    function personNotesSourceController($rootScope, $scope, $cookieStore, personService) {
+        var vm = this;
+        if (!$rootScope.UserName)
+            $rootScope.UserName = $cookieStore.get('username');
+        if (!$rootScope.pageSchema.Page)
+            $rootScope.pageSchema.Page = {};
+
+        $scope.pages = [
+            { Id: 0, Name: "Персона" },
+            { Id: 1, Name: "Поиск" },
+            { Id: 2, Name: "Мероприятие" },
+            { Id: 3, Name: "Площадка" },
+            { Id: 4, Name: "Профиль" },
+            { Id: 5, Name: "Главная" }
+        ];
+
+        //TODO get page blocks
+        $scope.bloks = [
+            { Id: null, Name: "Не указано" },
+            { Id: 1, Name: "Другие персоны подборки" }
+        ];
+        
+        $scope.getDetail = function (item) {
+            $scope.detailId = 0;
+            $scope.details = [];
+            switch (item) {
+                case 0:
+                    $scope.detailId = $rootScope.pageSchema.Page.IdPerson;
+                    return $rootScope.persons.forEach(function(item) {
+                        $scope.details.push({ Id: item.Id, Name: `${item.LastName} ${item.Name}` });
+                    });
+                case 1:
+                    return "Поиск";
+                case 2:
+                    $scope.detailId = $rootScope.pageSchema.Page.IdEvent;
+                    return $rootScope.events.forEach(function (item) {
+                        $scope.details.push({ Id: item.Id, Name: `${item.PageBlock.Page.Event.Name}` });
+                    });
+                case 3:
+                    $scope.detailId = $rootScope.pageSchema.Page.IdHall;
+                    return $rootScope.halls.forEach(function (item) {
+                        $scope.details.push({ Id: item.Id, Name: `${item.PageBlock.Page.Hall.Name}` });
+                    });
+                case 4:
+                    return "Профиль";
+                case 5:
+                    return "Главная";
+                default:
+                    return "";
+            }
+        }
+        $scope.getDetail($rootScope.pageSchema.Page.PageType);
+
+        $scope.changeDetail = function (item) {
+            switch ($rootScope.pageSchema.Page.PageType) {
+                case 0:
+                    $rootScope.pageSchema.Page.IdPerson = item;
+                    break;
+                case 1: break;
+                    return "Поиск";
+                case 2:
+                    $rootScope.pageSchema.Page.IdEvent = item;
+                    break;
+                case 3:
+                    $rootScope.pageSchema.Page.IdHall = item;
+                    break;
+                case 4: break;
+                    return "Профиль";
+                case 5: break;
+                    return "Главная";
+                default:
+                    return "";
+            }
+        }
+        $rootScope.userPageCategories = [
+            {
+                Id: null,
+                Name: "Не указано"
+            }
+        ];
+        personService.getUserPageCategories(function(data) {
+            $rootScope.userPageCategories.push.apply($rootScope.userPageCategories, data);
+        });
+
+        $rootScope.savePersonNotesSource = function() {
+            //TODO: save changes or create new source
+            //TODO: update notes list
+            //TODO: close this window
+            personService.saveDescriptionSchema($rootScope.editDescriptionId, $rootScope.pageSchema, function () {
+                $rootScope.getDescript();
+                app.closeView('disPersonNotesSource');
+            });
+        }
+
+        $scope.displayPersonWTF = function display_person_WTF() {
+            app.closeFive();
+            app.loadContentView('/main/dictionary/dictionary.person.wtf.html', 3200);
+        }
+            
+        $scope.changeCat = function (id) {
+            if ($rootScope.userPageCategories)
+                $rootScope.pageSchema.UserPageCategory = $rootScope.userPageCategories.filter(function(item) {
+                    return item.Id === id;
+                })[0].Name;
+        }
+            
+        $scope.cangeType = function (id) {
+            $rootScope.pageSchema.Type.Name = $scope.bloks.filter(function (item) {
+                return item.Id === id;
+            })[0].Name;
+        }
+    }
+
+    angular
+        .module('app')
+        .controller('personNotesSourceController', personNotesSourceController);
+
+    personNotesSourceController.$inject = ['$rootScope', '$scope', '$cookieStore', 'personService'];
+})();
