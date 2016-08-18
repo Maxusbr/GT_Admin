89b6cc80cd6e3b92c272d34af9ea59891065f41d
Alexander <alex.v.belozerov@gmail.com>
2016-08-18 22:13:59 +0300

    add event link event and person

---
+++ Getticket.Web.DAL/Entities/EventMediaLinkEvent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Getticket.Web.DAL.Entities
+{
+    /// <summary>
+    /// Связи между сущностями <see cref="Entities.EventMedia" /> и <see cref="Entities.Event" />
+    /// </summary>
+    public class EventMediaLinkEvent
+    {
+        /// <summary>
+        /// Внешний ключ для сущности <see cref="Entities.EventMedia"/>
+        /// </summary>
+        [Required]
+        [Key, Column(Order = 1)]
+        public int IdMedia { get; set; }
+
+        /// <summary>
+        /// <see cref="Entities.EventMedia"/>
+        /// </summary>
+        public virtual EventMedia Media { get; set; }
+
+        /// <summary>
+        /// Внешний ключ для <see cref="Entities.Event"/>
+        /// </summary>
+        [Required]
+        [Key, Column(Order = 2)]
+        public int IdEvent { get; set; }
+
+        /// <summary>
+        /// Событие <see cref="Entities.Event"/>
+        /// </summary>
+        public virtual Event Event { get; set; }
+
+    }
+}
--- Getticket.Web.DAL/IRepositories/IEventRepository.cs
+++ Getticket.Web.DAL/IRepositories/IEventRepository.cs
@@ -182,6 +182,20 @@ namespace Getticket.Web.DAL.IRepositories
         bool DeleteMediaType(int id);
 
         /// <summary>
+        /// Связать Media и Person
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool LinkMedia(MediaLinkPerson model);
+
+        /// <summary>
+        /// Связать Media и Event
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool LinkMedia(MediaLinkEvent model);
+
+        /// <summary>
         /// Возвращает количество описаний
         /// </summary>
         /// <param name="id"></param>
