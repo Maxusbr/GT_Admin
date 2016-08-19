38bc6e7dd3daed9427ef3e314e12ca73f39511c7
Maxusbr <max_73@inbox.ru>
2016-08-19 13:53:02 +0300

    fix edit event

--- AF/main/events/events.edit.js
+++ AF/main/events/events.edit.js
@@ -4,11 +4,25 @@
     function EventsEditController($rootScope, $scope, eventService, $filter) {
         var vm = this;
         $scope.event = $rootScope.editEvent;
-        eventService.getCategories(function (data) {
-            $rootScope.eventCategories = data;
-            $scope.baseCategory = data.filter(function (item) {
-                return item.IdParent == null;
+        if ($scope.event.DateStartSold)
+            $scope.dateStartSold = new Date($scope.event.DateStartSold);
+        if ($scope.event.DateStopSold)
+            $scope.dateStopSold = new Date($scope.event.DateStopSold);
+        $scope.$watch('dateStartSold', function (date) {
+            if (date) $scope.event.DateStartSold = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        });
+        $scope.$watch('dateStopSold', function(date) {
+            if (date) $scope.event.DateStopSold = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        });
+        if (!$rootScope.eventCategories) {
+            eventService.getCategories(function (data) {
+                $rootScope.eventCategories = data;
+                $scope.baseCategory = data.filter(function (item) {
+                    return item.IdParent == null;
+                });
             });
+        } else $scope.baseCategory = $rootScope.eventCategories.filter(function (item) {
+            return item.IdParent == null;
         });
         $scope.getChildCategory = function (id) {
             if (!$rootScope.eventCategories) return [];
@@ -25,6 +39,14 @@
         $scope.editOrg = function () {
             app.closeFive();
             app.loadContentView('/main/dictionary/dictionary.event.org.html', 3200);
+        }
+
+        $scope.saveEvent = function () {
+            eventService.Save($scope.event, function (data) {
+
+                app.closeView('eventEdit');
+            });
+            
         }
     }
 
