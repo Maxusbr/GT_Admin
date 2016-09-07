02c709ea2a534deb2077e1f2a24f23ad084750a2
gitwhatislove <whatislove0505@gmail.com>
2016-09-07 12:07:47 +0300

    'add_orderBy'

--- AF/main/events/events.list.js
+++ AF/main/events/events.list.js
@@ -28,5 +28,31 @@
             app.closeSecond();
             app.loadContentView('/main/events/events.viewone.html', 1800);
         }
+
+        $scope.order = '';
+        $scope.rever = false;
+        $scope.arrows = [false,false,false,false,false];
+        $scope.arrowId = ['Name','EventParentCategory','EventCategory','Organizer','AgeLimit'];
+        $scope.orders = function(name) {
+            if (name == $scope.order) {
+                if (!$scope.rever) {
+                    $scope.order = name;    
+                }
+                else { $scope.order = '-' + name; }
+                $scope.rever = !$scope.rever;
+            }
+            else {
+                $scope.order = name;
+                $scope.rever = true;
+            }
+
+            function arrowTrue(element, index, array) {
+                if ((element == name)&&(!$scope.rever)) $scope.arrows[index] = true;
+                else $scope.arrows[index] = false;
+            }
+
+            $scope.arrowId.forEach(arrowTrue);
+           
+        }
     }
 })();
--- AF/main/person/person.list.js
+++ AF/main/person/person.list.js
@@ -21,5 +21,30 @@
             app.closeSecond();
             app.loadContentView('/main/person/person.viewone.html', 1800);
         }
+        $scope.order = '';
+        $scope.rever = false;
+        $scope.arrows = [false,false,false,false,false];
+        $scope.arrowId = ['name.lastName','name.firstName','name.middleName','type','event'];
+        $scope.orders = function(name) {
+            if (name == $scope.order) {
+                if (!$scope.rever) {
+                    $scope.order = name;    
+                }
+                else { $scope.order = '-' + name; }
+                $scope.rever = !$scope.rever;
+            }
+            else {
+                $scope.order = name;
+                $scope.rever = true;
+            }
+
+            function arrowTrue(element, index, array) {
+                if ((element == name)&&(!$scope.rever)) $scope.arrows[index] = true;
+                else $scope.arrows[index] = false;
+            }
+
+            $scope.arrowId.forEach(arrowTrue);
+           
+        }
     }
 })();
