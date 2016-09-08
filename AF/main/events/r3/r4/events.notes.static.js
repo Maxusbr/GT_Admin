f5f9c22498cce784f35b003897b0a0b9135a5b42
Maxusbr <max_73@inbox.ru>
2016-09-08 08:35:53 +0300

    add tags to static description

--- AF/main/events/r3/r4/events.notes.static.js
+++ AF/main/events/r3/r4/events.notes.static.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    function eventsNotesStaticController($rootScope, $cookieStore, $scope, personService) {
+    function eventsNotesStaticController($rootScope, $cookieStore, $scope, $filter, eventService) {
         var vm = this;
         if (!$rootScope.UserName)
             $rootScope.UserName = $cookieStore.get('username');
@@ -10,14 +10,46 @@
             $rootScope.editableDesc;
         $scope.tizerId = $rootScope.editableDesc.id_DescriptionType === 1 ? $rootScope.editableDesc.Id : 0;
 
+        function saveTizerTags() {
+            if ($scope.tizerTags.length && $scope.staticDescription.Id > 0)
+                eventService.saveTags($scope.staticDescription.Id, $scope.tizerTags, 'tizer');
+        }
+
         $scope.savePersonNotesStatic = function () {
 
             $scope.staticDescription.id_Person = $rootScope.personId;
             $scope.staticDescription.id_DescriptionType = 2;
-            personService.saveEntity($scope.tizerId, $scope.staticDescription, 'description', function (data) {
+            eventService.saveEntity($scope.tizerId, $scope.staticDescription, 'description', function (data) {
+                $scope.staticDescription.Id = data;
+                saveTizerTags();
                 $rootScope.getDescript();
                 app.closeView('disPersonNotesStatic');
             });
+        }
+
+        // Tags
+        eventService.getTags(function (data) {
+            $scope.tags = [];
+            $scope.tags.push.apply($scope.tags, data);
+        });
+
+        function getTizerTags() {
+            if (!$scope.staticDescription || !$scope.staticDescription.Id) return;
+            eventService.getEntityTags($scope.staticDescription.Id, 'tizer', function (data) {
+                $scope.tizerTags = [];
+                $scope.tizerTags.push.apply($scope.tizerTags, data);
+            });
+        }
+        getTizerTags();
+
+
+        $scope.loadTags = function (query) {
+            if (!$scope.tags) return [];
+            var result = $scope.tags.filter(function (item) { return item.Name.toLowerCase().indexOf(query.toLowerCase()) >= 0; });
+            result = $filter('orderBy')(result, function (item) {
+                item.Name.substring(0, query.length);
+            });
+            return result;
         }
     }
 
@@ -25,5 +57,5 @@
         .module('app')
         .controller('eventsNotesStaticController', eventsNotesStaticController);
 
-    eventsNotesStaticController.$inject = ['$rootScope', '$cookieStore', '$scope', 'personService'];
+    eventsNotesStaticController.$inject = ['$rootScope', '$cookieStore', '$scope', '$filter', 'personService'];
 })();
--- AF/main/person/r3/r4/person.notes.static.js
+++ AF/main/person/r3/r4/person.notes.static.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    function personNotesStaticController($rootScope, $cookieStore, $scope, personService) {
+    function personNotesStaticController($rootScope, $cookieStore, $scope, $filter, personService) {
         var vm = this;
         if (!$rootScope.UserName)
             $rootScope.UserName = $cookieStore.get('username');
@@ -10,17 +10,49 @@
             $rootScope.editableDesc;
         $scope.tizerId = $rootScope.editableDesc.id_DescriptionType === 1 ? $rootScope.editableDesc.Id : 0;
 
+        function saveTizerTags() {
+            if ($scope.tizerTags.length && $scope.staticDescription.Id > 0)
+                personService.saveTags($scope.staticDescription.Id, $scope.tizerTags, 'tizer');
+        }
+
         $scope.savePersonNotesStatic = function () {
 
             $scope.staticDescription.id_Person = $rootScope.personId;
             $scope.staticDescription.id_DescriptionType = 2;
             personService.saveEntity($scope.tizerId, $scope.staticDescription, 'description', function (data) {
+                $scope.staticDescription.Id = data;
+                saveTizerTags();
                 $rootScope.getDescript();
                 if ($rootScope.getPersonCounts)
                     $rootScope.getPersonCounts();
                 app.closeView('disPersonNotesStatic');
             });
-            
+
+        }
+
+        // Tags
+        personService.getTags(function (data) {
+            $scope.tags = [];
+            $scope.tags.push.apply($scope.tags, data);
+        });
+
+        function getTizerTags() {
+            if (!$scope.staticDescription || !$scope.staticDescription.Id) return;
+            personService.getEntityTags($scope.staticDescription.Id, 'tizer', function (data) {
+                $scope.tizerTags = [];
+                $scope.tizerTags.push.apply($scope.tizerTags, data);
+            });
+        }
+        getTizerTags();
+
+
+        $scope.loadTags = function (query) {
+            if (!$scope.tags) return [];
+            var result = $scope.tags.filter(function (item) { return item.Name.toLowerCase().indexOf(query.toLowerCase()) >= 0; });
+            result = $filter('orderBy')(result, function (item) {
+                item.Name.substring(0, query.length);
+            });
+            return result;
         }
     }
 
@@ -28,5 +60,5 @@
         .module('app')
         .controller('personNotesStaticController', personNotesStaticController);
 
-    personNotesStaticController.$inject = ['$rootScope', '$cookieStore', '$scope', 'personService'];
+    personNotesStaticController.$inject = ['$rootScope', '$cookieStore', '$scope', '$filter', 'personService'];
 })();
